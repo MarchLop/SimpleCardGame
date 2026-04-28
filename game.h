723afe1bb5dcaa7b9895a3e377f63cb6414b4a5a
MarchLop <marchlop@malanc43.blog>
2026-04-28 22:58:03 +0800

    little update

--- game.h
+++ game.h
@@ -1,3 +1,4 @@
+#pragma once
 #include <vector>
 #include <utility>
 #include <random>
@@ -6,14 +7,14 @@
 
 std::random_device rd;
 std::mt19937 tt(rd()); 
-class card{
+class Card{
     public:
-    card(int n,int s):num(n),suit(s){}
+    Card(int n,int s):num(n),suit(s){}
     int num,suit;// spade=3,heart=2;diamond=1;clove=0
-    bool operator<(const card & other)const{
+    bool operator<(const Card & other)const{
         return (suit==other.suit)? num<other.num : suit<other.suit;
     }
-    bool operator==(const card &other)const{
+    bool operator==(const Card &other)const{
         return (num==other.num)&&(suit==other.suit);
     }
 };
@@ -23,12 +24,12 @@ class Deck{
     Deck(){
         for(int i=1;i<=13;i++){
             for(int j=0;j<=3;j++){
-                deck.push_back((card){i,j});
+                deck.push_back((Card){i,j});
             }
         }
     }
-    card Drawcard(){
-        card temp=deck.back();
+    Card Drawcard(){
+        Card temp=deck.back();
         deck.pop_back();
         return temp;
     }
@@ -36,17 +37,17 @@ class Deck{
         std::shuffle(deck.begin(), deck.end(), tt);
     }
     private:
-    std::vector<card> deck;
+    std::vector<Card> deck;
 };
 
 class GamePlayer{
     public:
-    void DarwCard(const card &d){
+    void DarwCard(const Card &d){
         hand.push_back(d);
         std::sort(hand.begin(),hand.end());
     }
-    bool PlayCard(const std::vector<card>& play){
-        for(const card &now : play){
+    bool PlayCard(const std::vector<Card>& play){
+        for(const Card &now : play){
             for(auto it = hand.begin(); it != hand.end(); ++it){
                 if(now == *it){
                     hand.erase(it);
@@ -60,14 +61,14 @@ class GamePlayer{
     }
     private:
     int id;
-    std::vector<card> hand;
+    std::vector<Card> hand;
 };
 
 class Game{
     public:
         std::shared_ptr<GamePlayer> AddPlayer(){
             auto newplayer = std::make_shared<GamePlayer>();
-            players.push_back(*newplayer);
+            players.push_back(newplayer);
             return newplayer;
         }
 
@@ -76,26 +77,26 @@ class Game{
             deck.shuffer();status=1;
             for(int j=0;j<13;++j)
             for(int i=0;i<4;++i){
-                players[i].DarwCard(deck.Drawcard());
+                players[i]->DarwCard(deck.Drawcard());
             }
             return 1;
         }
 
-        int rount(const std::vector<card>& play){// 0:error 1:continue 2:win
+        int round(const std::vector<Card>& play){// 0:error 1:continue 2:win
             if(!check(play))return 0;
             //card_in_table=
-            if(players[nowplayer].PlayCard(play))return 2;
+            if(players[nowplayer]->PlayCard(play))return 2;
             nowplayer++;
             nowplayer%=4;
             return 1;
         }
 
-        bool check(const std::vector<card>& play){
+        bool check(const std::vector<Card>& play){
             //...
             return 1;
         }
     public:
-        std::vector<GamePlayer> players;
+        std::vector<std::shared_ptr<GamePlayer>> players;
         bool status=0;
         int nowplayer=tt()%4;
         //??? card_in_table
